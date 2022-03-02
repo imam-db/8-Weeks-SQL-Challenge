04cb8d474da6e4c539ca4ad0e2662667c8e04b15
Imam <imam0067@gmail.com>
2022-03-02 19:36:59 +0700

    Week 3 - Customer Journey

---
+++ Week 3/Solution - Case Study Week 3.sql
@@ -0,0 +1,7 @@
+# A. CUstomer Journey
+## Dari total 8 customer:
+## - Kesemuanya dimulai dari trial dan berakhir dengan berlangganan.
+## - Total 3 customer akhirnya berlangganan basic monthly.
+## - Total 3 customer berlangganan pro monthly, 1 customer sebelumnya berlangganan basic monthly.
+## - Total 3 customer beralngganan pro annual, 1 customer sebelumnya berlangganan bascic monthly, 1 customer sebelumnya berlangganan pro monthly.
+## - Total 2 customer akhirnya berhenti berlangganan, 1 customer dari trial langsung berhenti, 1 customer sebelumnya sempat berlangganan pro monthly.
